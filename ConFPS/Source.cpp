1932a3634e235d5f535c5bec002f52281c4c79f6
joaqu <joaqu@JOAQUIMPC>
2024-11-14 21:48:07 -0400

    Add project files.

---
+++ ConFPS/Source.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+
+#include <Windows.h>
+
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
+float fPlayerX = 8.0f;
+float fPlayerY = 8.0f;
+float fPlayerA = 0.0f; // Angle of vision
+
+int nMapWidth = 16;
+int nMapHeight = 16;
+
+float fFOV = 3.14159 / 4.0;
+float fDepth = 16.0f;
+
+int main() {
+	// Screen buffer
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+	wstring map;
+	map += L"################";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"################";
+
+	// Game loop
+	while (1) {
+
+		for (int x = 0; x < nScreenWidth; x++) {
+			// For each column, calculate the projected ray angle into world space
+			float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)x / (float)nScreenWidth) * fFOV;
+			
+			float fDistanceToWall = 0;
+			bool bHitWall = false;
+			
+			float fEyeX = sinf(fRayAngle);
+			float fEyeY = cosf(fRayAngle);
+
+			while (!bHitWall && fDistanceToWall < fDepth) {
+				fDistanceToWall += 0.1f;
+
+				int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);
+				int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);
+			
+				// Test if ray is out of bounds
+				if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight) {
+					bHitWall = true;
+					fDistanceToWall = fDepth; // Set distance to maximum depth
+				}
+				else {
+					// Ray is inbounds so test to see if the ray cell is a wall block
+					if (map[nTestY * nMapWidth + nTestX] == '#') {
+						bHitWall = true;
+					}
+				}
+			}
+			// Calculate distance to ceiling and floor
+			int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float)fDistanceToWall);
+			int nFloor = nScreenHeight - nCeiling;		
+		
+			for (int y = 0; y < nScreenHeight; y++) {
+				if (y < nCeiling)
+					screen[y * nScreenWidth + x] = ' '; // Ceiling
+				else if(y > nCeiling && y <= nFloor)
+					screen[y * nScreenWidth + x] = '#'; // Wall
+				else
+					screen[y * nScreenWidth + x] = ' '; // Floor
+			}
+		}
+
+		
+		screen[nScreenWidth * nScreenHeight - 1] = '\0';
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+		
+	}
+	return 0;
+}
